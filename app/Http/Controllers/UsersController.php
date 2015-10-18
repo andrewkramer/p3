a566c2604144705d003b69af6f5d6cb350534b27
Andrew Kramer <ajkramer12@gmail.com>
2015-10-18 15:44:48 -0400

    Users POST Logic and Display

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Faker;
 
 class UsersController extends Controller {
 
@@ -10,13 +12,57 @@ class UsersController extends Controller {
     * Responds to requests to GET /users
     */
     public function getIndex() {
-        return view('users.index');
+		// create Faker instance
+		$faker = Faker\Factory::create();
+		
+		$userName = $faker->name();
+		$userStreet = $faker->streetAddress();
+		$userCity = $faker->city();
+		$userState = $faker->stateAbbr();
+		$userZip = $faker->postcode();
+		$userPhone = $faker->phoneNumber();
+		
+        return view('users.index')
+			->with('userName', $userName)
+			->with('userStreet', $userStreet)
+			->with('userCity', $userCity)
+			->with('userState', $userState)
+			->with('userZip', $userZip)
+			->with('userPhone', $userPhone);
     }
 
     /**
     * Responds to requests to POST /users
     */
-    public function postIndex() {
-        return "Users Custom";
+    public function postIndex(Request $request) {
+		// create Faker instance
+		$faker = Faker\Factory::create();
+		
+		$phone = $request->input('phone');
+		$address = $request->input('address');
+		
+		$userName = $faker->name();
+		$userCity = $faker->city();
+		$userState = $faker->stateAbbr();
+		if ($address == 1) {
+			$userStreet = $faker->streetAddress();
+			$userZip = $faker->postcode();
+		} else {
+			$userStreet = null;
+			$userZip = null;
+		}
+		if ($phone == 1) {
+			$userPhone = $faker->phoneNumber();
+		} else {
+			$userPhone = null;
+		}
+		
+        return view('users.index')
+			->with('userName', $userName)
+			->with('userStreet', $userStreet)
+			->with('userCity', $userCity)
+			->with('userState', $userState)
+			->with('userZip', $userZip)
+			->with('userPhone', $userPhone);
     }
 }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,26 +1,57 @@
 @extends('layouts.master')
 
-<?php
-	// create Faker instance
-	$faker = Faker\Factory::create();
-?>
-
 @section('header')
 	Random User Generator
 @stop
 
 @section('main')
-	<div class="col-md-12">
-		<div class="genResult">
-			<h2>
-				<?php
-					echo $faker->name() . "<br>";
-					echo $faker->streetAddress() . "<br>";
-					echo $faker->city() . ", " . $faker->stateAbbr() . " " . $faker->postcode() . "<br>";
-					echo $faker->phoneNumber() . "<br>";
-				?>
-			</h2>
-		</div>
+	<div class="genResult col-md-offset-3 col-md-6">
+		<h2>
+			{{ $userName }}<br>
+			@if(isset($userStreet))
+				{{ $userStreet }}<br>
+			@endif
+			{{ $userCity }}, {{ $userState }}
+			@if(isset($userStreet))
+				, {{ $userZip }}
+			@endif
+			<br>
+			{{ $userPhone }}<br>
+		</h2>
+	</div>
+	<div class="col-md-offset-3 col-md-6">
+		<form class="form" method="POST">
+			<input type='hidden' name='_token' value='{{ csrf_token() }}'>
+			<div class="form-group">
+				<label class="control-label">Address:</label>
+				<div class="radio">
+					<label for="Address1" class="col-md-12 control-label">
+						<input id="Address1" type="radio" name="address" value="1" checked>
+						Full
+					</label>
+					<label for="Address0" class="col-md-12 control-label">
+						<input id="Address0" type="radio" name="address" value="0">
+						City/State Only
+					</label>
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="control-label">Phone Number:</label>
+				<div class="radio">
+					<label for="Phone1" class="col-md-12 control-label">
+						<input id="Phone1" type="radio" name="phone" value="1" checked>
+						Yes
+					</label>
+					<label for="Phone0" class="col-md-12 control-label">
+						<input id="Phone0" type="radio" name="phone" value="0">
+						No
+					</label>
+				</div>
+			</div>
+			<div class="form-group">
+				<button class="btn btn-primary" type="submit" >Generate Text</button>
+			</div>
+		</form>
 		<h3>Random Users</h3>
 		<p>Use randomly generated user information to fill databases for testing.</p>
 	</div>
